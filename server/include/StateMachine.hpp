75664dbd284955aede7484db6f9cba6ef599d737
Alexis Crozet <alexis.crozet@epitech.eu>
2020-10-23 19:53:52 +0200

    feat(state_machine): added first version of state & statemachine with unit tests

---
+++ server/include/StateMachine.hpp
@@ -0,0 +1,35 @@
+#ifndef STATEMACHINE_HPP
+#define STATEMACHINE_HPP
+
+#include <stack>
+#include <memory>
+#include <optional>
+#include <vector>
+#include <functional>
+
+class AState {
+private:
+    short m_id;
+    static short m_stateCount;
+public:
+    AState();
+    short getId();
+    virtual void onStart() = 0;
+    virtual void onPause() = 0;
+    virtual void onResume() = 0;
+    virtual void onTick() = 0;
+    virtual void onStop() = 0;
+};
+
+class StateMachine {
+private:
+    std::stack<std::unique_ptr<AState>> m_statesStack;
+public:
+    StateMachine();
+    void setState(std::unique_ptr<AState> &state);
+    void stackState(std::unique_ptr<AState> &state);
+    std::optional<std::reference_wrapper<AState>> getCurrentState();
+    void leaveCurrentState();
+};
+
+#endif
--- server/source/main.cpp
+++ server/source/main.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
 int main(void) {
+    std::cout << "Hello..." << std::endl;
+    std::cout << "ZA WAAAAAAAAAARUDO" << std::endl;
     return 0;
 }
