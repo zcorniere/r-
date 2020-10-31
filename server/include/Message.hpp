6bda63f787ed7b294d6188f246f3cc2f30208845
Zacharie Corniere <zacharie.corniere@epitech.eu>
2020-10-31 22:37:18 +0100

    feat(Message): added concept for insert/extract method

--- server/include/Message.hpp
+++ server/include/Message.hpp
@@ -1,10 +1,14 @@
 #include <vector>
 #include <iostream>
 #include <cstring>
+#include <memory>
 #include <cstdint>
 
 #ifndef _MESSAGE_HPP_
 #define _MESSAGE_HPP_
+
+template<typename T>
+concept isLayoutStd = std::is_standard_layout<T>::value;
 
 template<typename T>
 struct MessageHeader {
@@ -26,18 +30,16 @@ class Message {
             return body.size();
         }
 
-        template<typename D>
+        template<isLayoutStd D>
         void insert(D &data) {
-            static_assert(std::is_standard_layout<D>::value, "This type requires specialisation");
             size_t i = body.size();
             body.resize(i + sizeof(D));
             std::memcpy(body.data() + i, &data, sizeof(D));
             head.size = body.size();
         };
         // extract data from the end of the buffer
-        template<typename D>
+        template<isLayoutStd D>
         void extract(D &data) {
-            static_assert(std::is_standard_layout<D>::value, "This type requires specialisation");
             size_t i = body.size() - sizeof(D);
             std::memcpy(&data, body.data() + i, sizeof(D));
             body.resize(i);
@@ -48,4 +50,13 @@ class Message {
         MessageHeader<T> head{};
         std::vector<uint8_t> body;
 };
+
+#include "interface/IClient.hpp"
+
+template<typename T>
+struct OwnedMsg {
+    T msg;
+    std::shared_ptr<ecs::IClient<T>> remote;
+};
+
 #endif //_MESSAGE_HPP_
