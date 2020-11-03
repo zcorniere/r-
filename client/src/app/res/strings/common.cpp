fd1725b8ad9db9168218cc2e9a2c51e144a4e283
Hugo PERINAZZO <Hugoperinazzopwolfdev.net>
2020-11-03 10:05:24 +0100

    removing useless assets

--- client/src/app/res/strings/common.cpp
+++ client/src/app/res/strings/common.cpp
@@ -15,10 +15,10 @@ CommonString::CommonString()
 {
     auto path = std::filesystem::current_path().string();
     list.emplace("full_path", path);    // TODO get absolute path of the exec instead of the current directory
-    list.emplace("foxy_head_trans_coffe", path + "/assets/icon/foxy_head_trans_redeye_coffe.png");
-    list.emplace("foxy_head_trans_gray", path + "/assets/icon/foxy_head_trans_gray3.png");
     list.emplace("helvetica_font", path + "/assets/fonts/helvetica/helvetica.ttf");
     list.emplace("foxy_red_eye", path + "/assets/icon/foxy_red_eye.png");
+    list.emplace("foxy_head_trans_coffe", path + "/assets/icon/foxy_head_trans_redeye_coffe.png");
+    list.emplace("foxy_head_trans_gray", path + "/assets/icon/foxy_head_trans_gray3.png");
     list.emplace("Loader_Blue", path + "/assets/loader/blue.png");
     list.emplace("Loader_Green", path + "/assets/loader/green.png");
 }
