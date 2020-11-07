2651afbd6441ef275da890e7189c2e77da9b2ac9
Zacharie Corniere <zacharie.corniere@epitech.eu>
2020-11-07 11:30:30 +0100

    feat(AssetServer): thow execption if the config path is invalid

--- server/source/AssetsServer.cpp
+++ server/source/AssetsServer.cpp
@@ -18,6 +18,8 @@ AssetsServer::AssetsServer(const unsigned port, const std::string &path):
     long id = 0;
     for (const auto &e: std::filesystem::directory_iterator(path)) {
         auto path = e.path();
+        if (!ext_to_type.contains(path.extension()))
+            continue;
         assets_map.insert({id++, {path.string(), ext_to_type.at(path.extension())}});
     }
     this->start();
@@ -38,15 +40,19 @@ void AssetsServer::onMessage(Message<protocol::tcp::AssetsRequest> msg) {
                     break;
                 }
                 protocol::tcp::AssetsPackage reply;
-                reply.data = this->getFileAt(assets_map.at(body->id).first);
-                reply.config = this->getFileAt(this->getConfigForAssets(assets_map.at(body->id).first));
-                if (reply.data.size() != 0 && reply.config.size() != 0) {
-                    reply.size_data = reply.data.size();
-                    reply.size_config = reply.config.size();
-                    reply.id = body->id;
-                    reply.type = assets_map.at(body->id).second;
-                    rep.head.code = msg.head.code;
-                    rep.insert(reply);
+                try {
+                    reply.data = this->getFileAt(assets_map.at(body->id).first);
+                    reply.config = this->getFileAt(this->getConfigForAssets(assets_map.at(body->id).first));
+                    if (reply.data.size() != 0 && reply.config.size() != 0) {
+                        reply.size_data = reply.data.size();
+                        reply.size_config = reply.config.size();
+                        reply.id = body->id;
+                        reply.type = assets_map.at(body->id).second;
+                        rep.head.code = msg.head.code;
+                        rep.insert(reply);
+                    }
+                } catch (const std::runtime_error &re) {
+                    std::cerr << "[SERVER]: Execption " << re.what() << std::endl;
                 }
             } break;
             default: break;
@@ -70,7 +76,12 @@ std::vector<uint8_t> AssetsServer::getFileAt(const std::string &path) {
 }
 
 std::string AssetsServer::getConfigForAssets(std::string path) {
-    size_t dot = path.find_last_of('0');
-    path.insert(dot, "_config");
+    size_t dot = path.find_last_of('.');
+    if (dot == std::string::npos)
+        throw std::runtime_error("not dot");
+    path.replace(dot, 12, "_config.json");
+    if (!std::filesystem::exists(path)) {
+        throw std::runtime_error("Not a valid path");
+    }
     return path;
 }
