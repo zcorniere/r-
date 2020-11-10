b1936a1d8a29d4d38c4bc5b23d77cfcfeb97cfbf
Alexis Crozet <alexis.crozet@epitech.eu>
2020-11-11 00:44:44 +0100

    feat(draft): scrolling background !

--- draft/source/LevelState.cpp
+++ draft/source/LevelState.cpp
@@ -2,6 +2,7 @@
 #include "LevelState.hpp"
 #include "components/Sprite.hpp"
 #include "components/PlayerControlled.hpp"
+#include "components/Velocity.hpp"
 
 void LevelState::onStart(Game &instance)
 {
@@ -9,12 +10,13 @@ void LevelState::onStart(Game &instance)
     instance.componentStorage.buildEntity()
         .withComponent(Sprite("level1", 0))
         .withComponent(Transform(Dimensional(10, 10), Dimensional(1, 1), Dimensional(2, 2)))
+        .withComponent(Velocity(-0.5, 0))
         .build();
 
     // Creating Player Ship
     instance.componentStorage.buildEntity()
         .withComponent(Sprite("player_ships", 0))
-        .withComponent(Transform(Dimensional(10, 10), Dimensional(1, 1), Dimensional(1, 1)))
+        .withComponent(Transform(Dimensional(10, 10), Dimensional(1, 1), Dimensional(1.5, 1.5)))
         .withComponent(PlayerControlled{1})
         .build();
 }
