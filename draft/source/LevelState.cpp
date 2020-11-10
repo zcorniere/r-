8547057e95b0ba7ce3954e27784ce135e5d0375f
Alexis Crozet <alexis.crozet@epitech.eu>
2020-11-10 23:51:00 +0100

    fix(level_state): readjusting sprite order

--- draft/source/LevelState.cpp
+++ draft/source/LevelState.cpp
@@ -5,17 +5,17 @@
 
 void LevelState::onStart(Game &instance)
 {
+    // Creating Back ground
+    instance.componentStorage.buildEntity()
+        .withComponent(Sprite("level1", 0))
+        .withComponent(Transform(Dimensional(10, 10), Dimensional(1, 1), Dimensional(2, 2)))
+        .build();
+
     // Creating Player Ship
     instance.componentStorage.buildEntity()
         .withComponent(Sprite("player_ships", 0))
         .withComponent(Transform(Dimensional(10, 10), Dimensional(1, 1), Dimensional(1, 1)))
         .withComponent(PlayerControlled{1})
-        .build();
-
-    // Creating Back ground
-    instance.componentStorage.buildEntity()
-        .withComponent(Sprite("level1", 0))
-        .withComponent(Transform(Dimensional(10, 10), Dimensional(1, 1), Dimensional(2, 2)))
         .build();
 }
 
