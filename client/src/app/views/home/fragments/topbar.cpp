7d0936e0c98479a7c17bc83e91a03f93da7840ed
Hugo PERINAZZO <Hugoperinazzopwolfdev.net>
2020-11-15 16:01:01 +0100

    edit debug default ip entry

--- client/src/app/views/home/fragments/topbar.cpp
+++ client/src/app/views/home/fragments/topbar.cpp
@@ -83,7 +83,8 @@ TopBar::TopBar(std::optional<std::string> &intent_ref, bidimensional::Transform 
 void TopBar::onCreateView()
 {
 //    ip_entry->setText("10.62.105.147:25555");   // TODO temp code for debug
-    ip_entry->setText("127.0.0.1:25555");   // TODO temp code for debug
+    ip_entry->setText("10.62.105.183:25555");   // TODO temp code for debug
+//    ip_entry->setText("127.0.0.1:25555");   // TODO temp code for debug
 }
 
 void TopBar::onUpdateView()
