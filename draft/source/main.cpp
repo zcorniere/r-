04097ae9260a4d3c45ace3d84f30ce8d2bd54bd4
Julien Philippon <julien.philippon@epitech.eu>
2020-11-15 20:41:11 +0100

    refactor(game): R-Type initalization function
    Now all the systems, components and first game state of the R-Type are
    created in a function which is independent of the game engine setup.

--- draft/source/main.cpp
+++ draft/source/main.cpp
@@ -1,13 +1,11 @@
 #include "Game.hpp"
 #include "SfmlAudioModule.hpp"
 #include "SfmlModule.hpp"
-#include "LevelState.hpp"
+#include "load_game.hpp"
+#include "components/CollisionBox.hpp"
 #include <iostream>
-#include "systems/InputHandler.hpp"
-#include "systems/rtype_systems.h"
 #include <boost/dll/runtime_symbol_info.hpp>
 #include <cstdlib>
-#include <time.h>
 
 int main(void)
 {
@@ -36,66 +34,8 @@ int main(void)
     game.addModule("audio-sfml", std::move(audio_module));
     game.setAudioModule("audio-sfml");
 
-    srand(time(NULL));
-
-    /*
-    ** COMPONENTS
-    ** entity's'datas containers
-    */
-
-    game.componentStorage.registerComponent<Transform>();
-    game.componentStorage.registerComponent<Sprite>();
-    game.componentStorage.registerComponent<PlayerControlled>();
-    game.componentStorage.registerComponent<Velocity>();
-    game.componentStorage.registerComponent<CollisionBox>();
-    game.componentStorage.registerComponent<GameObject>();
-    game.componentStorage.registerComponent<PlayerShipController>();
-    game.componentStorage.registerComponent<PatternLoop>();
-    game.componentStorage.registerComponent<AnimationLoop>();
-    game.componentStorage.registerComponent<Destructible>();
-    game.componentStorage.registerComponent<AnimMontage>();
-    game.componentStorage.registerComponent<DeathMontage>();
-    game.componentStorage.registerComponent<WaveCannon>();
-    game.componentStorage.registerComponent<Trajectory>();
-    game.componentStorage.registerComponent<BydoShooter>();
-    game.componentStorage.registerComponent<Lifetime>();
-    game.componentStorage.registerComponent<OrientedSprite>();
-    game.componentStorage.registerComponent<PlayerScanner>();
-    game.componentStorage.registerComponent<ShootMontage>();
-    game.componentStorage.registerComponent<RestrictionBox>();
-    game.componentStorage.registerComponent<EnemyGroup>();
-
-    /*
-    ** SYSTEMS
-    ** components's logic functions
-    */
-
-    // sprite_displayer
-    game.systemStorage.addSystem([](IDisplayModule &display,
-                                    const Transform &transform,
-                                    const Sprite &sprite) {
-        if (!sprite.hidden)
-            display.drawSprite(sprite.name, transform, sprite.tile_id);
-    });
-
-    // reaper
-    std::function<void(Game &)> destructible_reaper_system = destructible_reaper;
-    game.systemStorage.addSystem(destructible_reaper_system);
-    game.systemStorage.addSystem(corpse_hider);
-    std::function<void(Game &)> lifetime_reaper_system = lifetime_reaper;
-    game.systemStorage.addSystem(lifetime_reaper_system);
-    game.systemStorage.addSystem(lifetime_ager);
-
-    // playership
-    game.systemStorage.addSystem(playership_animations);
-    game.systemStorage.addSystem(playership_ct_input_getter);
-    game.systemStorage.addSystem([]
-    (Velocity &velocity, const PlayerShipController &controller) {
-        velocity.x = controller.moovingRight * controller.getSpeed() \
-        + controller.moovingLeft * -1 * controller.getSpeed();
-        velocity.y = controller.moovingDown * controller.getSpeed() \
-        + controller.moovingUp * -1 * controller.getSpeed();
-    });
+    // Load the R-Type Game
+    load_game(game);
 
     // DEBUG dollision displayer
     game.systemStorage.addSystem([]
@@ -105,50 +45,6 @@ int main(void)
         rect.setSize(sf::Vector2f(box.width * transform.scale.x, box.height * transform.scale.y))    ;
         dynamic_cast<SfmlModule &>(display).drawDebugBox(rect);
     });
-
-    // collisions
-    std::function<void(Game &)> collision_system = collisions_update;
-    game.systemStorage.addSystem(collision_system);
-    game.systemStorage.addSystem(collision_damages);
-
-    // anim montages
-    game.systemStorage.addSystem(play_deathmontages);
-    game.systemStorage.addSystem(draw_animmontages);
-    game.systemStorage.addSystem(draw_deathmontages);
-    game.systemStorage.addSystem(draw_shootmontages);
-
-    game.systemStorage.addSystem(run_animation_loops);
-    game.systemStorage.addSystem(run_pattern_loops);
-    game.systemStorage.addSystem(select_oriented_sprite);
-
-    // wave cannon
-    std::function<void(Game &)> wave_cannon_projectile_system = wave_cannon_projectile_summoner;
-    game.systemStorage.addSystem(wave_cannon_montage_player);
-    game.systemStorage.addSystem(wave_cannon_projectile_system);
-    game.systemStorage.addSystem(wave_cannon_input_getter);
-
-    // movement
-    game.systemStorage.addSystem(velocity_applicator);
-    game.systemStorage.addSystem(trajectory_applicator);
-    game.systemStorage.addSystem(restriction_applicator);
-
-    // bydo shooter
-    std::function<void(Game &)> bydo_shooter_projectile_system = bydo_shooter_projectile_summoner;
-    game.systemStorage.addSystem(bydo_shooter_projectile_system);
-    game.systemStorage.addSystem(bydo_charger);
-    std::function<void(Game &)> player_scanner_detector_system = player_scanner_detector;
-    game.systemStorage.addSystem(player_scanner_detector_system);
-    game.systemStorage.addSystem(bydo_shooter_apply_scanner);
-
-    game.systemStorage.addSystem(run_enemy_group);
-
-    /*
-    ** STATES
-    ** game related logic
-    */
-
-    std::unique_ptr<AState> level_state(new LevelState);
-    game.stateMachine.setState(std::move(level_state));
 
     game.run();
 }
---
+++ server/source/load_game.cpp
@@ -0,0 +1,118 @@
+#include "load_game.hpp"
+#include "LevelState.hpp"
+#include "components/PlayerControlled.hpp"
+#include "systems/rtype_systems.h"
+
+#include <time.h>
+
+void load_game(Game &game)
+{
+    srand(time(NULL));
+
+    /*
+    ** COMPONENTS
+    ** entity's'datas containers
+    */
+
+    game.componentStorage.registerComponent<Transform>();
+    game.componentStorage.registerComponent<Sprite>();
+    game.componentStorage.registerComponent<PlayerControlled>();
+    game.componentStorage.registerComponent<Velocity>();
+    game.componentStorage.registerComponent<CollisionBox>();
+    game.componentStorage.registerComponent<GameObject>();
+    game.componentStorage.registerComponent<PlayerShipController>();
+    game.componentStorage.registerComponent<PatternLoop>();
+    game.componentStorage.registerComponent<AnimationLoop>();
+    game.componentStorage.registerComponent<Destructible>();
+    game.componentStorage.registerComponent<AnimMontage>();
+    game.componentStorage.registerComponent<DeathMontage>();
+    game.componentStorage.registerComponent<WaveCannon>();
+    game.componentStorage.registerComponent<Trajectory>();
+    game.componentStorage.registerComponent<BydoShooter>();
+    game.componentStorage.registerComponent<Lifetime>();
+    game.componentStorage.registerComponent<OrientedSprite>();
+    game.componentStorage.registerComponent<PlayerScanner>();
+    game.componentStorage.registerComponent<ShootMontage>();
+    game.componentStorage.registerComponent<RestrictionBox>();
+    game.componentStorage.registerComponent<EnemyGroup>();
+
+    /*
+    ** SYSTEMS
+    ** components's logic functions
+    */
+
+    // sprite_displayer
+    game.systemStorage.addSystem([](IDisplayModule &display,
+                                    const Transform &transform,
+                                    const Sprite &sprite) {
+        if (!sprite.hidden)
+            display.drawSprite(sprite.name, transform, sprite.tile_id);
+    });
+
+    // reaper
+    std::function<void(Game &)> destructible_reaper_system =
+        destructible_reaper;
+    game.systemStorage.addSystem(destructible_reaper_system);
+    game.systemStorage.addSystem(corpse_hider);
+    std::function<void(Game &)> lifetime_reaper_system = lifetime_reaper;
+    game.systemStorage.addSystem(lifetime_reaper_system);
+    game.systemStorage.addSystem(lifetime_ager);
+
+    // playership
+    game.systemStorage.addSystem(playership_animations);
+    game.systemStorage.addSystem(playership_ct_input_getter);
+    game.systemStorage.addSystem(
+        [](Velocity &velocity, const PlayerShipController &controller) {
+            velocity.x = controller.moovingRight * controller.getSpeed() +
+                         controller.moovingLeft * -1 * controller.getSpeed();
+            velocity.y = controller.moovingDown * controller.getSpeed() +
+                         controller.moovingUp * -1 * controller.getSpeed();
+        });
+
+    // collisions
+    std::function<void(Game &)> collision_system = collisions_update;
+    game.systemStorage.addSystem(collision_system);
+    game.systemStorage.addSystem(collision_damages);
+
+    // anim montages
+    game.systemStorage.addSystem(play_deathmontages);
+    game.systemStorage.addSystem(draw_animmontages);
+    game.systemStorage.addSystem(draw_deathmontages);
+    game.systemStorage.addSystem(draw_shootmontages);
+
+    game.systemStorage.addSystem(run_animation_loops);
+    game.systemStorage.addSystem(run_pattern_loops);
+    game.systemStorage.addSystem(select_oriented_sprite);
+
+    // wave cannon
+    std::function<void(Game &)> wave_cannon_projectile_system =
+        wave_cannon_projectile_summoner;
+    game.systemStorage.addSystem(wave_cannon_montage_player);
+    game.systemStorage.addSystem(wave_cannon_projectile_system);
+    game.systemStorage.addSystem(wave_cannon_input_getter);
+
+    // movement
+    game.systemStorage.addSystem(velocity_applicator);
+    game.systemStorage.addSystem(trajectory_applicator);
+    game.systemStorage.addSystem(restriction_applicator);
+
+    // bydo shooter
+    std::function<void(Game &)> bydo_shooter_projectile_system =
+        bydo_shooter_projectile_summoner;
+    game.systemStorage.addSystem(bydo_shooter_projectile_system);
+    game.systemStorage.addSystem(bydo_charger);
+    std::function<void(Game &)> player_scanner_detector_system =
+        player_scanner_detector;
+    game.systemStorage.addSystem(player_scanner_detector_system);
+    game.systemStorage.addSystem(bydo_shooter_apply_scanner);
+
+    game.systemStorage.addSystem(run_enemy_group);
+
+    /*
+    ** STATES
+    ** game related logic
+    */
+
+    std::unique_ptr<AState> level_state(new LevelState);
+    game.stateMachine.setState(std::move(level_state));
+}
