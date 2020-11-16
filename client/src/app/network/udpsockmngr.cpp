c56d56969231521f3c5434f15be83b6b74c20e41
Hugo PERINAZZO <Hugoperinazzopwolfdev.net>
2020-11-16 20:59:32 +0100

    adding debug print

--- client/src/app/network/udpsockmngr.cpp
+++ client/src/app/network/udpsockmngr.cpp
@@ -52,6 +52,25 @@ void network::UdpSockMngr::send(protocol::MessageToSend<UdpCode> message)
     buffer.resize(length);
     std::memcpy(buffer.data(), &message.head, sizeof(message.head));
     std::memcpy(buffer.data() + sizeof(message.head), message.body.data(), message.head.body_size);
+
+    if (message.head.code == protocol::udp::Code::Input) {
+        auto keys_size = static_cast<std::uint32_t>(sizeof(protocol::input::KeysEvent) * protocol::input::keys_array_size);
+        std::cout << std::dec << std::endl;
+        std::cout << "I send Input: buffer.size(): " << std::dec << buffer.size() << " | length: " << length << std::endl;
+        std::cout << "total sizeof : " << sizeof(protocol::udp::from_client::Input::nb_keys) + keys_size + sizeof(protocol::udp::from_client::Input::pos) << std::endl;
+        std::cout << "message.head.firstbyte  : " <<  static_cast<int>(message.head.firstbyte) << std::endl;
+        std::cout << "message.head.secondbyte : " << static_cast<int>(message.head.secondbyte) << std::endl;
+        std::cout << "message.head.code       : " << std::hex << static_cast<int>(message.head.code) << std::endl;
+        std::cout << "message.head.body_size  : " << std::dec << static_cast<int>(message.head.body_size) << std::endl;
+        std::cout << "body:" << std::hex;
+        for (auto i = 0; i < message.head.body_size; ++i) {
+            std::cout << int(message.body[i]) << " ";
+        }
+        std::cout << std::dec << std::endl;
+        std::cout << "cast in protocol::Input:";
+    }
+
+
     socket.send_to(boost::asio::buffer(buffer, length), endpoint);
 }
 
